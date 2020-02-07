fd46dcaf0821bca6df17a85ce8ce3219303c3fdf
Patrick Allison <dbarawn@gmail.com>
2020-02-07 17:40:42 -0500

    add KEEP attribute to ICAP because synthesis doesn't realize it's important

--- rtl/spi_bootload.v
+++ rtl/spi_bootload.v
@@ -223,6 +223,7 @@ module spi_bootload( input clk_i,
     assign icap_in[15:8] = reverse_byte(arg0[15:8]);
     assign icap_in[23:16] = reverse_byte(arg1[7:0]);
     assign icap_in[31:24] = reverse_byte(arg1[15:8]);    
+    (* KEEP = "TRUE" *)
     ICAPE2 #(.DEVICE_ID(IDCODE),.ICAP_WIDTH("X32"),.SIM_CFG_FILE_NAME("NONE")) 
         u_icap(.I(icap_in),.O(),.CSIB(icap_csib),.RDWRB(icap_rdwrb),.CLK(clk_i));
         
